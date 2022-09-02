deea9627d7565aad97215f20f1ce5c9706adc572
smj0503 <wovj0503@gmail.com>
2022-09-02 17:12:00 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// DB
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'nalm',
+  password : 'smjcau2017!',
+  //database : 'test'
+  database : 'nalm'
+});
+
+// React와 연동
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
+
+// 서버 접속 확인
+app.listen(3001, ()=>{
+  console.log('listening on 3001')
+});
+
+connection.connect((err)=>{
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+app.get('/data', (req, res)=>{
+  const sql = "select * from restaurant"
+  //const sql = "select * from beta"
+  connection.query(sql, (err, result, fields)=>{
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'nalm/build')));
+
+// app.get('/', (req, res)=>{
+//   res.sendFile(path.join(__dirname, '/nalm/build/index.html'));
+// });
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/nalm/build/index.html'));
+});
